314e4be8c8aff9fa9837a6bcf5aeaa58cc1b1313
galmeida <gero.alm@gmail.com>
2023-03-15 17:28:23 -0300

    primer commit

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent {
+
+  constructor(private service:GifsService){}
+
+  @ViewChild('txtBuscar') busqueda!:ElementRef<HTMLInputElement>;
+
+  buscar(){
+    const valor =  this.busqueda.nativeElement.value;
+
+    this.service.buscarGifs(valor);
+
+    this.busqueda.nativeElement.value="";
+  }
+}
